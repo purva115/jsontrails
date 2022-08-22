9997f9a8592f6b5540d341e09b841435fbeb4de0
PURVA JAGTAP <48603493+purva115@users.noreply.github.com>
2022-08-22 18:30:32 +0530

    header added APIi working

--- crack_postreq.py
+++ crack_postreq.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import csv
 import requests
 import os
@@ -7,34 +8,38 @@ header = {
     "User-Agent": "Mozilla/5.0",
 }
 URL = 'https://dummy.restapiexample.com/api/v1/create'
-PARAMS = {"name": "test", "salary": "", "age": "23"}
+PARAMS = {"name": "", "salary": "", "age": ""}
 
-file_name = "sample.csv"
+file_name = "sample2.csv"
 if os.stat(file_name).st_size == 0:
     print('File is empty')
     PARAMS = {"name": "test", "salary": "*", "age": "23"}
-    r = requests.post(url=URL, params=PARAMS)
-
+    r = requests.post(URL, json=PARAMS,headers=header)
     print(r.status_code)
-    print(r.text)
+    # print(r.text)
 
 else:
     print('File is not empty')
-    with open('sample.csv', mode='r') as file:
+    with open('sample2.csv', mode='r') as file:
         csvFile = csv.reader(file)
         for line in csvFile:
-            PARAMS["salary"] = line[0]
+            PARAMS["name"] = line[0]
+            PARAMS["salary"] = line[1]
+            PARAMS["age"]=line[2]
             NEW_PARAMS = json.dumps(PARAMS)  # ' ' -> " "
-            print("PARAMS: ", PARAMS)
+            # print("PARAMS: ", PARAMS)
             res = json.loads(NEW_PARAMS)
-            print(type(PARAMS))
-            print(type(NEW_PARAMS))
-            print(type(res))
-            print("NEW PARAMS: ", NEW_PARAMS)
+            # print(type(PARAMS))
+            # print(type(NEW_PARAMS))
+            # print(type(res))
+            # print("NEW PARAMS: ", NEW_PARAMS)
             # print(line)
             # POST REQUEST
             # print(URL)
             r = requests.post(URL, json=res,headers=header)
+            print(str(r.status_code)+" : "+r.reason)
            
-            print(r.status_code)
-            print(r.text)
+            
+
+            
+          
